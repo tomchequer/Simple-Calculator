153f00e2f832f75c0428e4f7c6fede5871dd910d
antonio <antoniochequer@gmail.com>
2021-08-02 17:27:20 -0300

    finished the core of the program. Working release.

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,2 +1,47 @@
 #This will be a simple text-based calculator
 #built for practice, but its also very functional.
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def sub(n1, n2):
+    return n1 - n2
+
+def mult(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add, 
+    "-": sub, 
+    "*": mult,
+    "/": div
+    }
+
+
+n1 = int(input('Whats the first number?'))
+n2 = int(input('Whats the second number?'))
+
+for i in operations:
+    print(i) 
+
+op = input('Which operations above would you like to make?')
+
+opfunction = ''
+
+for i in operations:
+    if i == op:
+        opfunction = operations[i]
+
+
+        
+answer = opfunction(n1, n2)
+
+
+
+print(f"{n1} {op} {n2} = {answer}")
