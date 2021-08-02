ca48718061b2bb5a500d7d8802f8c5e599f358f6
antonio <antoniochequer@gmail.com>
2021-08-02 17:54:15 -0300

    finished polishing and improved UX and functionalities in general.

--- simplecalculator.py
+++ simplecalculator.py
@@ -23,25 +23,54 @@ operations = {
     "/": div
     }
 
+print("""
+  _____________________
+|  _________________  |
+| | WELCOME         | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|     
+      
+      """)
 
-n1 = int(input('Whats the first number?'))
-n2 = int(input('Whats the second number?'))
+def calculator():
 
-for i in operations:
-    print(i) 
+    n1 = float(input('Whats the first number?'))
 
-op = input('Which operations above would you like to make?')
-
-opfunction = ''
-
-for i in operations:
-    if i == op:
-        opfunction = operations[i]
-
-
+    for i in operations:
+            print(i) 
         
-answer = opfunction(n1, n2)
+    turnedon = True
 
+    while turnedon:
+        
+        op = input('Which operations above would you like to make?')
 
+        n2 = float(input('Whats the next number?'))
 
-print(f"{n1} {op} {n2} = {answer}")
+        opfunction = ''
+
+        for i in operations:
+            if i == op:
+                opfunction = operations[i]
+                
+        answer = opfunction(n1, n2)
+
+        print(f"{n1} {op} {n2} = {answer}")
+        
+        if input ('Do you wanna make another operation? ("y" to keep going or "n" to restart) ') == "y":
+            n1 = answer
+        else:
+            turnedon = False
+            calculator()
+
+calculator()
+
